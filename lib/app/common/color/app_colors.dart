13980592bc13b668af3b08c1b0f22d0885c24ec2
Kevin-Sarges <kevinsarges08@gmail.com>
2023-07-13 16:09:22 -0300

    feat: cores e rotas do app

--- lib/app/common/color/app_colors.dart
+++ lib/app/common/color/app_colors.dart
@@ -1,1 +1,7 @@
-class AppColors {}
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color bluePrimary = const Color(0xFF003352);
+  static Color blueSecondary = const Color(0xFF00C0FE);
+  static Color white = const Color(0xFFFFFFFF);
+}
