ecbc79e56546f894fb55154d2ae88dd79af33f29
Kevin-Sarges <kevinsarges08@gmail.com>
2023-07-13 19:16:21 -0300

    feat: inject

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:image_genius/app/features/home/domain/inject/inject_dependency.dart';
 import 'package:image_genius/app/my_app.dart';
 
 void main() async {
@@ -11,5 +12,6 @@ void main() async {
   ]);
   await FlutterConfig.loadEnvVariables();
 
+  InjectDependency.init();
   runApp(const MyApp());
 }
