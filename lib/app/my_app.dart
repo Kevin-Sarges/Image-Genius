bbae65efc46131412569e536352b213b44b2dda3
Kevin-Sarges <kevinsarges08@gmail.com>
2023-07-13 16:10:04 -0300

    feat: config

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_genius/app/common/color/app_colors.dart';
 import 'package:image_genius/app/common/routes/app_routes.dart';
 import 'package:image_genius/app/features/home/presenter/home_screen.dart';
+import 'package:image_genius/app/features/splash/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,8 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Image Genius',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.bluePrimary,
+      ),
+      debugShowCheckedModeBanner: false,
       routes: {
-        AppRoutes.initial: (context) => const HomeScreen(),
+        AppRoutes.initial: (context) => const SplashScreen(),
+        AppRoutes.home: (context) => const HomeScreen(),
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:image_genius/app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   await FlutterConfig.loadEnvVariables();
 
   runApp(const MyApp());
